7a2cc54cfa66208a8a4a84e0f97039f4fc4a3939
LucasDev <LucasDev9645@gmail.com>
2023-05-17 20:09:31 -0300

    Creating routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./Home";
+import GameList from "./GameList";
+import GameDetails from "./GameDetails";
 
 const App = () => {
   return (
-    <div>
-      <Home />
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="games" element={<GameList />} />
+          <Route path="game-details/:gameId" element={<GameDetails />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 };
 
